8cda04f93d57e2addb7ea2dd5da454b3bda343a9
Gordon O'Neill <oneill.gordon@gmail.com>
2019-11-03 23:11:38 -0500

    tweak sizes

--- Console/Console/main.cpp
+++ Console/Console/main.cpp
@@ -8,15 +8,15 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QGraphicsScene * scene = new QGraphicsScene();
     Spaceship * spaceship = new Spaceship();
-    spaceship->setRect(50,50,100,100);
+    spaceship->setRect(0,0,100,100);
     scene->addItem(spaceship);
     spaceship->setFlag(QGraphicsItem::ItemIsFocusable);
     spaceship->setFocus();
 
     QGraphicsView * view = new QGraphicsView(scene);
     //view->setStyleSheet("background-color:black;");
-    view->setFixedSize(1920, 1080);
-    scene->setSceneRect(0,0,1920,1080);
+    view->setFixedSize(1900, 1060);
+    scene->setSceneRect(0,0,1900,1060);
     spaceship->setPos(view->width()/2-spaceship->rect().width()/2,
                       view->height()-spaceship->rect().height()-10);
     view->showFullScreen();
